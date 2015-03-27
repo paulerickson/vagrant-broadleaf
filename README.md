7d203cd04ca2dade96968c69968384c5a494dd30
Paul Erickson <paul.erickson@wisc.edu>
2015-03-27 17:17:22 +0100

    Fix up tomcat setup and update readme

--- README.md
+++ README.md
@@ -15,18 +15,10 @@ Warning: The virtual machine that will be provisioned by vagrant will have 2 GB 
 3. Clone or download this repository to the root of your project directory: `git clone https://github.com/nblair/vagrant-broadleaf.git`
 4. In your project directory, run `vagrant up`
 
-Be prepared to wait; the first launch of the site may take a good 15 minutes.
+Be prepared to wait; the first launch of the site may take around 5 minutes.
 
 ### Using
 
-To access the site, point your browser at [http://localhost:8080](http://localhost:8080).
-To access the admin console, point your browser at [https://localhost:8444/admin](https://localhost:8444/admin). Log in with username *admin* and password *admin*.
-
-### Debugging
-
-The first lauch can take a very long time to start up, but if something does seem amiss, you can check that the service is running and look at its log for problems.
-
-1. Use `vagrant ssh` to login to the virtual machine.
-2. Verify that broadleaf is running: `pgrep -lf jetty`
-3. Its output can be found in /vagrant/eclipse-workspace/DemoSite/site/stdout.log
+To access the site, point your browser at [http://localhost:8080/mycompany](http://localhost:8080/mycompany).
+To access the admin console, point your browser at [http://localhost:8080/admin](http://localhost:8080/admin). Log in with username *admin* and password *admin*.
 
--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -1,23 +1,39 @@
 class tomcat {
 
   package { 'tomcat7':
-    ensure => installed
+    ensure => installed,
   }
   service { 'tomcat7':
     ensure => running,
-    require => Package['tomcat7']
+    enable => true,
+    require => Package['tomcat7'],
+    subscribe => [File['/usr/share/tomcat7/bin/setenv.sh'], File['/var/lib/tomcat7/webapps/ROOT.war']]
   }
+  file { '/usr/share/tomcat7/bin/setenv.sh':
+    content => 'export CATALINA_OPTS="-Xms512m -Xmx512m"',
+    ensure => file,
+    require => Package['tomcat7'],
+  }
+
+  #Webapps
   #file { "/var/lib/tomcat7/webapps":
   #  source => "puppet:///modules/tomcat/files/webapps",
-  #  require => Service['tomcat7']
+  #  require => Package['tomcat7'],
   #}
-  file { "/var/lib/tomcat7/webapps/mycompany.war":
-    source => "/vagrant/modules/tomcat/files/webapps/mycompany.war",
-    require => Service['tomcat7']
+  # Clobber the default root app before deploying ours
+  file { "/var/lib/tomcat7/webapps/ROOT":
+    ensure => absent,
+    force => true, # must force to delete a directory
+    require => Package['tomcat7'],
+    before => File['/var/lib/tomcat7/webapps/ROOT.war'],
+  }
+  file { "/var/lib/tomcat7/webapps/ROOT.war":
+    source => "/vagrant/modules/tomcat/files/webapps/ROOT.war",
+    require => Package['tomcat7'],
   }
   file { "/var/lib/tomcat7/webapps/admin.war":
     source => "/vagrant/modules/tomcat/files/webapps/admin.war",
-    require => Service['tomcat7']
+    require => Package['tomcat7'],
   }
 
 }
